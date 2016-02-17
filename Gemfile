aeb65559244959218221068064879b55ca7d3ffd
Jorge Naranjo <j.naranjojr@gmail.com>
2016-02-17 13:21:56 -0500

    Ability to add, edit, and destroy

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'haml', '~> 4.0', '>= 4.0.7'
 gem 'simple_form', '~> 3.2', '>= 3.2.1'
+gem 'better_errors', '~> 2.1', '>= 2.1.1'
 
 gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -26,9 +26,16 @@ before_action :find_recipe, only: [:show, :edit, :update, :destroy]
   end
 
   def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @recipe.destroy
+    redirect_to root_path, notice: "Successfully deleted recipe."
   end
 
   private
